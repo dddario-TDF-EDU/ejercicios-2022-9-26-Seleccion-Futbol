1ba85c5d02b82fcda8a9c8bd0b21246dcb56ec62
= <dariocabrera@tdf.edu.ar>
2022-10-02 21:10:16 -0300

    añadida clase entrenador

---
+++ claseEntrenador.ts
@@ -0,0 +1,60 @@
+import { Futbolista } from "./claseFutbolista.js";
+import { Persona } from "./clasePersona.js";
+
+export {
+    Persona
+}from "./clasePersona.js";
+
+export {
+    Futbolista
+}from "./claseFutbolista.js";
+
+class Entrenador extends Persona {
+    private titulares: Futbolista[];
+    private suplentes: Futbolista[];
+
+    constructor(auxName : string, auxID : number, auxTitulares: Futbolista[], auxSuplentes: Futbolista[], auxSueldo?: number){
+        if (auxSueldo === undefined){
+            super(auxName, auxID);
+        } else {
+            super(auxName, auxID, auxSueldo);
+        }
+
+        this.titulares = auxTitulares;
+        this.suplentes = auxSuplentes;
+    }
+
+    private addTitular(index: number): void {
+        this.titulares.push(this.suplentes[index]);
+        this.suplentes.splice(index,1);
+    }
+
+    private removeTitular(index: number): void {
+        this.suplentes.push(this.titulares[index])
+        this.titulares.splice(index,1);
+    }
+
+    public getTitulares(): void {
+        for (let i = 0; i < this.titulares.length; i++ ) {
+            console.log ( i + "_ Jugador " + this.titulares[i].getName())
+        }
+        let readlineSync = require('readline-sync');
+        let decision : number = readlineSync.question("Si desea eliminar uno de los titulares ingrese el numero correspondiente.");
+        if (decision <= this.titulares.length && decision >= 0) {
+            this.removeTitular(decision);
+            console.log ("Cambio realizado")
+        }
+    }
+
+    public getSuplentes(): void {
+        for (let i = 0; i < this.titulares.length; i++ ) {
+            console.log ( i + "_ Jugador " + this.suplentes[i].getName())
+        }
+        let readlineSync = require('readline-sync');
+        let decision : number = readlineSync.question("Si desea ascender uno de los suplentes ingrese el numero correspondiente.");
+        if (decision <= this.suplentes.length && decision >= 0) {
+            this.addTitular(decision);
+            console.log ("Cambio realizado")
+        }
+    }
+}
